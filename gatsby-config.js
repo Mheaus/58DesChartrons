d5162e3fcdc0b943242a238a6bc14d2fa2246e8e
mathieu audebert <mathieu.adbrt@gmail.com>
2018-09-08 14:34:09 +0200

    add infos in package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     /*
      * Gatsby's data processing layer begins with “source”
      * plugins.  You can source data nodes from anywhere but
--- src/components/RoomList/RoomList.jsx
+++ src/components/RoomList/RoomList.jsx
@@ -1,6 +1,22 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
+const Angle = props => (
+  <svg
+    aria-hidden="true"
+    data-prefix="far"
+    data-icon="angle-right"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 192 512"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M187.8 264.5L41 412.5c-4.7 4.7-12.3 4.7-17 0L4.2 392.7c-4.7-4.7-4.7-12.3 0-17L122.7 256 4.2 136.3c-4.7-4.7-4.7-12.3 0-17L24 99.5c4.7-4.7 12.3-4.7 17 0l146.8 148c4.7 4.7 4.7 12.3 0 17z"
+    />
+  </svg>
+)
+
 export default class RoomList extends PureComponent {
   constructor(props) {
     super(props)
@@ -28,45 +44,36 @@ export default class RoomList extends PureComponent {
     const { className, onImageClick, rooms } = this.props
     const { openRoomName } = this.state
 
+    console.log(rooms)
+
     return (
       <div className={`room-list ${className}`}>
-        {Object.entries(rooms).map(([key, images]) => (
+        {Object.entries(rooms).map(([roomName, images]) => (
           <div
-            className={`room-list__room room ${key}${
-              openRoomName === key ? ' room--opened' : ''
+            className={`room-list__room room ${roomName}${
+              openRoomName === roomName ? ' room--opened' : ''
             }`}
-            key={key}
+            key={roomName}
           >
             <button
               className="room__title-expand-item"
-              onClick={() => this.toggleOpen(key)}
+              onClick={() => this.toggleOpen(roomName)}
               type="button"
             >
-              {key.split('_').join(' ')}
-              <svg
-                aria-hidden="true"
-                data-prefix="far"
-                data-icon="angle-right"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 192 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M187.8 264.5L41 412.5c-4.7 4.7-12.3 4.7-17 0L4.2 392.7c-4.7-4.7-4.7-12.3 0-17L122.7 256 4.2 136.3c-4.7-4.7-4.7-12.3 0-17L24 99.5c4.7-4.7 12.3-4.7 17 0l146.8 148c4.7 4.7 4.7 12.3 0 17z"
-                />
-              </svg>
+              {roomName.split('_').join(' ')}
+              <Angle />
             </button>
             <div className="room__items images">
               {images.map((image, index) => (
                 <div
                   className={`images__item images__item${index + 1}`}
-                  key={key}
+                  key={roomName}
                   onClick={() => onImageClick(image)}
                   onKeyPress={() => onImageClick(image)}
                   role="button"
                   tabIndex="0"
                 >
-                  <img src={image} alt={`${key}${index + 1}`} />
+                  <img src={image} alt={`${roomName}${index + 1}`} />
                 </div>
               ))}
             </div>
