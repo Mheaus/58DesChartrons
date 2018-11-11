649849e1657972b46801d3633dd2f18ee35117b3
mathieu audebert <mathieu.adbrt@gmail.com>
2018-11-11 21:47:53 +0100

    feat : add netflify tag in contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -60,8 +60,9 @@ export default class Contact extends Component {
 
         <form
           className={`contact__form${isFormOpen ? ' contact__form--open' : ''}`}
-          action="https://formspree.io/mathieu.adbrt@gmail.com"
           method="POST"
+          data-netlify="true"
+          netlify
         >
           <label className="contact__form__email" htmlFor="_replyto">
             <span>Votre Email :</span>
